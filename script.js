a91f9c4cd4d1f0e5f36013d1acad955191f5be15
nhardiansa <nhardiansa@gmail.com>
2021-02-15 22:31:18 +0800

    memperbagus interaksi pada tampilan mobile

---
+++ script.js
@@ -0,0 +1,32 @@
+const menuIcon = document.querySelector("nav span")
+const close = document.querySelector(".nav-menu span")
+const navbar = document.querySelector("nav")
+const navList = close.parentElement
+const link = navList.querySelectorAll("ul li a")
+
+
+// When menu icon clicked
+menuIcon.addEventListener("click", function(){
+    navList.classList.toggle("display-none")
+})
+
+// When close icon clicked
+close.addEventListener("click", () => {
+    navList.classList.toggle("display-none")
+})
+
+// When page on scroll
+window.addEventListener("scroll", function(){
+    if(window.pageYOffset > navbar.offsetTop){
+        navbar.classList.add('scrolled')
+    }else{
+        navbar.classList.remove('scrolled')
+    }
+})
+
+// When link on nav list clicked
+link.forEach(e => {
+    e.addEventListener("click", function(){
+        navList.classList.toggle("display-none")
+    })
+});
