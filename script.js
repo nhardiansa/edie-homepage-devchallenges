e25cd305466b446cafcf4cd7af67a73037535259
nhardiansa <nhardiansa@gmail.com>
2021-02-17 22:24:11 +0800

    menyelesaikan seluruh versi dan memperbaiki interaksi

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+// Nav
 const menuIcon = document.querySelector("nav span")
 const close = document.querySelector(".nav-menu span")
 const navbar = document.querySelector("nav")
 const navList = close.parentElement
 const link = navList.querySelectorAll("ul li a")
+
+// Services
+const card = document.querySelectorAll(".service .cards .card")
+card.forEach(e => {
+    e.addEventListener("mouseover", function(){
+        e.classList.add("hover")
+    })
+
+    e.addEventListener("mouseout", function(){
+        e.classList.remove("hover")
+    })
+});
 
 
 // When menu icon clicked
